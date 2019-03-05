a1ee5386ad6921179ad12a026a235af21079424c
Janessa Baja <janessa.comanda.baja@gmail.com>
2019-03-05 23:24:09 +0800

    feat(question-2): change dialog to popover

--- app/Controller/FormatController.php
+++ app/Controller/FormatController.php
@@ -2,20 +2,20 @@
 	class FormatController extends AppController{
 		
 		public function q1(){
-			
 			$this->setFlash('Question: Please change Pop Up to mouse over (soft click)');
-				
-			
-// 			$this->set('title',__('Question: Please change Pop Up to mouse over (soft click)'));
 		}
 		
-		public function q1_detail(){
-
+		public function q1_detail() {
 			$this->setFlash('Question: Please change Pop Up to mouse over (soft click)');
-				
-			
-			
-// 			$this->set('title',__('Question: Please change Pop Up to mouse over (soft click)'));
+		}
+
+		public function q1_success() {
+			// Redirect to q1 if no selection is found, usually this scenario will happen if the user manually
+			// types in the q1_success url in the browser
+			if (!isset($this->data['Type'])) {
+				return $this->redirect(array("controller" => "format", "action" => "q1"));
+			}
+			$this->setFlash('Question: Please change Pop Up to mouse over (soft click)');
 		}
 		
 	}
---
+++ app/View/Format/q1_success.ctp
@@ -0,0 +1,9 @@
+<div class="row-fluid">
+	<div class="alert alert-info">
+		<h3>Form Successfully Submitted</h3>
+
+		<h4>
+			Thank you for choosing: <b><?php echo $this->data['Type']['type'] ?></b>
+		</h4>
+	</div>
+</div>
