f637a47005e619b01bf3533d4c915dd8b9154669
Janessa Baja <janessa.comanda.baja@gmail.com>
2019-03-05 23:19:58 +0800

    feat(question-1): optimize listing record page

--- app/Controller/RecordController.php
+++ app/Controller/RecordController.php
@@ -1,43 +1,91 @@
 <?php
 	class RecordController extends AppController{
-		
+	
+		public $components = array('Paginator');
+	
 		public function index(){
 			ini_set('memory_limit','256M');
 			set_time_limit(0);
-			
+
 			$this->setFlash('Listing Record page too slow, try to optimize it.');
-			
-			
-			$records = $this->Record->find('all');
-			
-			$this->set('records',$records);
-			
-			
+
 			$this->set('title',__('List Record'));
 		}
-		
-		
-// 		public function update(){
-// 			ini_set('memory_limit','256M');
-			
-// 			$records = array();
-// 			for($i=1; $i<= 1000; $i++){
-// 				$record = array(
-// 					'Record'=>array(
-// 						'name'=>"Record $i"
-// 					)			
-// 				);
-				
-// 				for($j=1;$j<=rand(4,8);$j++){
-// 					@$record['RecordItem'][] = array(
-// 						'name'=>"Record Item $j"		
-// 					);
-// 				}
-				
-// 				$this->Record->saveAssociated($record);
-// 			}
-			
-			
-			
-// 		}
+
+		/**
+		 * Retrieves the records list based on the information provided by the datatables
+		 *
+		 * @return json the json response required by datatables
+		 */
+		public function listings()
+		{
+			$this->autoRender = false;
+			$this->Paginator->settings = $this->getPaginationSettings($this->data);
+			$data = $this->Paginator->paginate('Record');
+			$totalCount = $this->params->paging['Record']['count'];
+
+			$pagination = array(
+				"iTotalRecords" => $totalCount,
+				"iTotalDisplayRecords" => $totalCount,
+				"aaData" => $this->toDatatableData($data)
+			);
+			return json_encode($pagination);
+		}
+
+		/**
+		 * Generates a pagination settings based on the POST params
+		 *
+		 * @param array $data The POST params
+		 * @return array $settings Array of configuration settings.
+		 */
+		private function getPaginationSettings($data) {
+			/**
+			 * For the SQL part optimization, only select fields that are needed for the table on our view.
+			 * Also change the type of the `name` field to VARCHAR and specify a length which is 15
+			 * since the max name that we have as of the moment is 'Record 1000' that has length which is < 15.
+			 * I'm assuming that `name` field should not be duplicated so we need to add index to it by making it
+			 * a unique field 
+			 */
+			$sortDir = isset($data['sSortDir_0']) ? $data['sSortDir_0']: 'asc';
+			$searchKeyword = isset($data['sSearch']) ? $data['sSearch'] : '';
+			$limit = isset($data['iDisplayLength']) ? $data['iDisplayLength'] : 10;
+			$page = isset($data['iDisplayStart']) ? $data['iDisplayStart'] : 1;
+			$order = isset($data['iSortCol_0']) ? $data['iSortCol_0'] : '0';
+			$orderList = array(
+				'0' => array('Record.id' => $sortDir),
+				'1' => array('Record.name' => $sortDir),
+			);
+
+			$settings = array(
+				'fields' => array('Record.id', 'Record.name'),
+				'limit' => $limit,
+				'page' => $page,
+				'order' => $orderList[$order],
+				'recursive' => 0,
+				'conditions' => array(
+					array(
+						'OR' => array(
+							'Record.id LIKE' => "%{$searchKeyword}%",
+							'Record.name LIKE' => "%{$searchKeyword}%"
+						)
+					)
+				)
+			);
+			return $settings;
+		}
+
+		/**
+		 * Transforms Model query results into an Array of records
+		 *
+		 * @param array $records Model query results
+		 * @return array list of records with id and name only
+		 */
+		private function toDatatableData($records) {
+			return array_map(function($record) {
+				return array(
+					$record['Record']['id'],
+					$record['Record']['name']
+				);
+			}, $records);
+		}
 	}
--- app/View/Record/index.ctp
+++ app/View/Record/index.ctp
@@ -1,4 +1,3 @@
-
 <div class="row-fluid">
 	<table class="table table-bordered" id="table_records">
 		<thead>
@@ -7,22 +6,18 @@
 				<th>NAME</th>	
 			</tr>
 		</thead>
-		<tbody>
-			<?php foreach($records as $record):?>
-			<tr>
-				<td><?php echo $record['Record']['id']?></td>
-				<td><?php echo $record['Record']['name']?></td>
-			</tr>	
-			<?php endforeach;?>
-		</tbody>
+		<tbody></tbody>
 	</table>
 </div>
 <?php $this->start('script_own')?>
 <script>
-$(document).ready(function(){
-	$("#table_records").dataTable({
-
-	});
-})
+	$(document).ready(function(){
+		$("#table_records").dataTable({
+			"bProcessing": true,
+			"bServerSide": true,
+			"sServerMethod": "POST",
+			"sAjaxSource": "Record/listings"
+		});
+	})
 </script>
 <?php $this->end()?>
